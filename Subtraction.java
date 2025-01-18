ded32011c8fd38e9e28a2b90e86126d7cab9781a
1AbhishekKumar <abhishek.kumar_cs21@gla.ac.in>
2025-01-18 12:18:10 +0530

    Calc

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 Numbers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a-b);
+        sc.close();
+    }
+}
